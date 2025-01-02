3fd1c56533a65426945a51126cc8a258ddbf7ef7
yesdopepe <laiskashkash33@gmail.com>
2025-01-03 00:23:47 +0300

    added swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('chatty backend')
+    .setDescription('NestJS Chat API')
+    .setVersion('1.0')
+    .addTag('chat')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.enableCors();
   await app.listen(5000);
 }
